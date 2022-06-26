0841408d9119c92d53a7b9c09f9b166a3fa66ba2
Roman Pavlovskyi <rpavlovskyi@gmail.com>
2022-06-26 07:33:39 -0400

    Add example .env

--- client/components/Paper.tsx
+++ client/components/Paper.tsx
@@ -16,7 +16,7 @@ import NextLink from 'next/link'
 import ReactPDF from 'react-pdf'
 
 const PAPER_TO_PUBLICATION_ID: { [key: string]: string } = {
-  '1': '0' + 'x84f7-0x04',
+  '1': '0' + 'x84f7-0x05',
 }
 
 const Paper = () => {
@@ -64,7 +64,7 @@ const Paper = () => {
           <Heading color={'red.500'} pb={6}>
             You have to claim the publication first on{' '}
             <Link
-              href="https://lenster.xyz/posts/0x84f7-0x04"
+              href="https://lenster.xyz/posts/0x84f7-0x05"
               color={'green.400'}
               textDecoration="underline"
             >
